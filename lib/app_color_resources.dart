b074d2a21471d7322a62d76067b957567b2f6f76
shortmikelon <kenesaset485@gmail.com>
2025-02-10 18:15:42 +0500

    Renamed to app_color_resources.dart and added new colors

--- lib/app_color_resources.dart
+++ lib/app_color_resources.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ChangeClientNameConstants {
-  static const String studentTitle = 'Ученик';
-  static const String clientTitle = 'Клиент';
-  static const String otherTitle = 'Другое';
-
-  static const String clientNameLabel = 'Название клиента';
-
+class AppColorResources {
+  static const black = Color(0xFF000000);
+  static const black87 = Color(0xDD000000);
   static const blue = Color(0xFF0062FF);
   static const lightBlue = Color(0xFF1890FF);
   static const spitsbergenBlue = Color(0x336F757D);
   static const darkGray = Color(0xFF595959);
   static const lightGray = Color(0xFF8C8C8C);
   static const lightGray20 = Color(0xFFD9D9D9);
+  static const white = Color(0xFFFFFFFF);
+  static const mainBackgroundColor = Color(0xFFF5F5F5);
+
+  static const errorRed = Color(0xDDD0343C);
 }
