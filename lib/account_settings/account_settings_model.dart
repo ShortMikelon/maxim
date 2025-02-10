1c38a23a3d0bfcf7c73f64bf560fb39058dfb00f
shortmikelon <kenesaset485@gmail.com>
2025-02-10 17:19:58 +0500

    Added AccountSettings page

---
+++ lib/account_settings/account_settings_model.dart
@@ -0,0 +1,109 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:maxim/data_sources/remote_storage.dart';
+import 'package:maxim/data_sources/secure_storage.dart';
+
+class AccountSettingsModel with ChangeNotifier {
+  final FocusNode usernameFocusNode = FocusNode();
+  final FocusNode phoneNumberFocusNode = FocusNode();
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+
+  final _storage = SecureStorage();
+  final _remoteDataSource = RemoteDataSource();
+
+  String _profession = '';
+
+  bool _isLoading = true;
+
+  AccountSettingsModel() {
+    usernameFocusNode.addListener(notifyListeners);
+    phoneNumberFocusNode.addListener(notifyListeners);
+
+    phoneNumberController.addListener(_formatPhoneNumber);
+
+    init();
+  }
+
+  void init() async {
+    usernameController.text = await _storage.getUsername() ?? '';
+    phoneNumberController.text = (await _storage.getPhoneNumber() ?? '');
+    _profession = (await _remoteDataSource.fetchTrainerData()).prof;
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void saveData() {
+    _storage.setUsername(usernameController.text);
+
+    _storage.setPhoneNumber(phoneNumberController.text);
+  }
+
+  @override
+  void dispose() {
+    usernameFocusNode.removeListener(notifyListeners);
+    usernameController.dispose();
+    usernameFocusNode.dispose();
+
+    phoneNumberFocusNode.removeListener(notifyListeners);
+    phoneNumberController.dispose();
+    phoneNumberFocusNode.dispose();
+
+    super.dispose();
+  }
+
+  String get profession => _profession;
+
+  bool get isLoading => _isLoading;
+
+  void _formatPhoneNumber() {
+    String text = phoneNumberController.text.replaceAll(RegExp(r'\D'), '');
+
+    log('format phone number called. text: $text, phoneNumberController: ${phoneNumberController.text}');
+
+    if (text.isEmpty) {
+      if (phoneNumberController.text.isNotEmpty) {
+        phoneNumberController.text = '';
+      }
+      return;
+    }
+
+    bool startsWithEight = text.startsWith("8");
+    bool startsWithSevenOrPlus = text.startsWith("7") || text.startsWith("+");
+    if (!startsWithEight) {
+      text = "+7${text.substring(1)}";
+      log('777: $text');
+    } else {
+      log('888: $text');
+    }
+
+
+    if (startsWithEight && text.length > 11) {
+      text = text.substring(0, 11);
+    } else if (startsWithSevenOrPlus && text.length > 12) {
+      text = text.substring(0, 12);
+    }
+
+    String formattedText = startsWithEight ? "8" : "+7";
+    int offset = startsWithEight ? 1 : 2;
+
+    if (text.length > offset) formattedText += " ${text.substring(offset, text.length >= offset + 3 ? offset + 3 : text.length)}";
+    if (text.length > offset + 3) formattedText += " ${text.substring(offset + 3, text.length >= offset + 6 ? offset + 6 : text.length)}";
+    if (text.length > offset + 6) formattedText += " ${text.substring(offset + 6, text.length >= offset + 8 ? offset + 8 : text.length)}";
+    if (text.length > offset + 8) formattedText += " ${text.substring(offset + 8)}";
+
+
+    phoneNumberController.value = TextEditingValue(
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: formattedText.length),
+    );
+  }
+}
+
+enum AccountSettingsField {
+  username,
+  phoneNumber,
+}
---
+++ lib/account_settings/account_settings_page.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/material.dart';
+import 'package:maxim/app_string_resources.dart';
+import 'package:maxim/change_password/change_password_page.dart';
+import 'package:maxim/widgets/app_button.dart';
+import 'package:maxim/widgets/app_shimmer.dart';
+import 'package:maxim/widgets/app_text_styles.dart';
+import 'package:provider/provider.dart';
+
+import 'account_settings_model.dart';
+
+class AccountSettingsPage extends StatelessWidget {
+  const AccountSettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _AccountSettingsAppBar(),
+      body: _AccountSettingsBody(),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
+
+class _AccountSettingsAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
+      title: const Text(
+        AppStringResources.accountSettings,
+        style: AppTextStyles.appBarTextStyle,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AccountSettingsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AccountSettingsModel(),
+      child: Consumer<AccountSettingsModel>(
+        builder: (context, model, child) => Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: model.isLoading
+              ? _AccountSettingsPendingBody()
+              : _AccountSettingsDisplayBody(
+                  usernameController: model.usernameController,
+                  phoneNumberController: model.phoneNumberController,
+                  usernameFocusNode: model.usernameFocusNode,
+                  phoneNumberFocusNode: model.phoneNumberFocusNode,
+                  profession: model.profession,
+                  onSaveButtonPressed: model.saveData,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AccountSettingsPendingBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(children: [
+          AppShimmer(
+            child: Container(
+              height: 56,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          AppShimmer(
+            child: Container(
+              height: 56,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          AppShimmer(
+            child: Container(
+              height: 56,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          AppShimmer(
+            child: Container(
+              height: 56,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ]),
+        AppShimmer(
+          child: Container(
+            height: 56,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _AccountSettingsDisplayBody extends StatelessWidget {
+  final TextEditingController usernameController;
+  final TextEditingController phoneNumberController;
+  final String profession;
+  final FocusNode usernameFocusNode;
+  final FocusNode phoneNumberFocusNode;
+  final void Function() onSaveButtonPressed;
+
+  const _AccountSettingsDisplayBody({
+    required this.usernameController,
+    required this.phoneNumberController,
+    required this.usernameFocusNode,
+    required this.phoneNumberFocusNode,
+    required this.profession,
+    required this.onSaveButtonPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            _AccountSettingsInputField(
+              label: 'Имя',
+              controller: usernameController,
+              focusNode: usernameFocusNode,
+            ),
+            const SizedBox(height: 8),
+            _AccountSettingsInputField(
+              label: 'Телефонный номер',
+              controller: phoneNumberController,
+              focusNode: phoneNumberFocusNode,
+              keyboardType: TextInputType.phone,
+            ),
+            const SizedBox(height: 8),
+            _AccountSettingsPasswordButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const ChangePasswordPage(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 8),
+            _AccountSettingsProfessionButton(profession: profession),
+          ],
+        ),
+        AppButton(
+          background: const Color(0xFF0062FF),
+          onPressed: onSaveButtonPressed,
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text('Сохранить', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _AccountSettingsInputField extends StatelessWidget {
+  final String label;
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final TextInputType keyboardType;
+
+  const _AccountSettingsInputField({
+    required this.label,
+    required this.controller,
+    required this.focusNode,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final borderColor =
+        focusNode.hasFocus ? const Color(0xFF0062FF) : Colors.white;
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: borderColor, width: 2),
+        color: Colors.white,
+      ),
+      child: TextField(
+        cursorHeight: 20,
+        cursorWidth: 1,
+        cursorColor: const Color(0xFF0062FF),
+        keyboardType: keyboardType,
+        controller: controller,
+        focusNode: focusNode,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(0),
+          border: InputBorder.none,
+          labelText: label,
+          labelStyle: AppTextStyles.hintTextStyle,
+          filled: true,
+          fillColor: Colors.white,
+          suffixIcon: const Icon(
+            Icons.border_color,
+            size: 18.75,
+            color: Color(0xFF8C8C8C),
+          ),
+        ),
+        style: AppTextStyles.blackTextStyle,
+      ),
+    );
+  }
+}
+
+class _AccountSettingsPasswordButton extends StatelessWidget {
+  final void Function() onPressed;
+
+  const _AccountSettingsPasswordButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppButton(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      background: Colors.white,
+      onPressed: onPressed,
+      child: const Align(
+        alignment: Alignment.center,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Text('Пароль', style: AppTextStyles.mainTextStyle),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              size: 17,
+              color: Color(0xFF8C8C8C),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AccountSettingsProfessionButton extends StatelessWidget {
+  final String profession;
+
+  const _AccountSettingsProfessionButton({
+    required this.profession,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppButton(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      background: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text('Профессия', style: AppTextStyles.mainTextStyle),
+                Text(profession, style: AppTextStyles.hintTextStyle),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.arrow_forward_ios,
+            size: 17,
+            color: Color(0xFF8C8C8C),
+          ),
+        ],
+      ),
+      onPressed: () {},
+    );
+  }
+}
